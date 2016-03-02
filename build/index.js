e922d6ea8e52e484f32ddd5f330e2ede7a32130e
Matthew Caldwell <caldwellysr@gmail.com>
2016-03-01 23:44:42 -0500

    play again scene

--- build/index.js
+++ build/index.js
@@ -7927,11 +7927,25 @@
 
 	module.exports = function(ecs, game) {
 		ecs.addEach(function(entity, elapsed) {
+			
 			var size = {
 				"width": game.canvas.width * 0.5,
 				"height": game.canvas.height * 0.1
 			};
 			game.entities.set(entity, "size", size);
+			var position = game.entities.get(entity, "position");
+			position.x = game.canvas.width / 2 - size.width / 2;
+			position.y = game.canvas.height / 2 - size.height / 2;
+
+			if(game.input.mouse.consumePressed(0)) {
+				if (game.input.mouse.x > position.x 
+					&& game.input.mouse.y > position.y 
+					&& game.input.mouse.x < position.x + size.width 
+					&& game.input.mouse.y < position.y + size.height) {
+						game.switchScene("main");
+					}
+			}
+
 		}, "play_again");
 	};
 
@@ -8038,7 +8052,6 @@
 	"use strict";
 
 	module.exports = function(game) { // eslint-disable-line no-unused-vars
-		game.switchScene("main");
 	};
 
 
@@ -8239,6 +8252,7 @@
 				"id": 1,
 				"name": "play_again",
 				"play_again": true,
+				"strokeStyle": "red",
 				"position": {
 					"x": 10,
 					"y": 10
--- src/systems/simulation/play_again.js
+++ src/systems/simulation/play_again.js
@@ -2,10 +2,24 @@
 
 module.exports = function(ecs, game) {
 	ecs.addEach(function(entity, elapsed) {
+		
 		var size = {
 			"width": game.canvas.width * 0.5,
 			"height": game.canvas.height * 0.1
 		};
 		game.entities.set(entity, "size", size);
+		var position = game.entities.get(entity, "position");
+		position.x = game.canvas.width / 2 - size.width / 2;
+		position.y = game.canvas.height / 2 - size.height / 2;
+
+		if(game.input.mouse.consumePressed(0)) {
+			if (game.input.mouse.x > position.x 
+				&& game.input.mouse.y > position.y 
+				&& game.input.mouse.x < position.x + size.width 
+				&& game.input.mouse.y < position.y + size.height) {
+					game.switchScene("main");
+				}
+		}
+
 	}, "play_again");
 };
